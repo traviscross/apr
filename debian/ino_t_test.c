640da047fd167e5a32ecfbc7cc2efc6988565c11
Peter Samuelson <peter@p12n.org>
2006-11-13 05:07:20 +0000

    Update to r282 due to alpha being different: ino_t is 32 bits.  On other Debian platforms, it is the same as unsigned long.
    Thanks to Steve Langasek for the (in retrospect, obvious) suggestion of
    just using an #ifdef.  Here I was set to go into full autoconf mode.
    
    
    git-svn-id: svn+ssh://svn.debian.org/svn/pkg-apache/trunk/apr@286 01b336ce-410b-0410-9a02-a0e7f243c266

--- debian/ino_t_test.c
+++ debian/ino_t_test.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include <sys/types.h>
 
+/* keep this in sync with the patch to apr_file_info.h */
+#ifdef __alpha__
+typedef unsigned int              apr_ino_t;
+#else
+typedef unsigned long int         apr_ino_t;
+#endif
+
 int main (void)
 {
-	if (sizeof(ino_t) == sizeof(long))
+	size_t s0 = sizeof(ino_t), s1 = sizeof(apr_ino_t);
+	if (s0 == s1)
 		return 0;
-	fprintf(stderr, "*** 'ino_t' size is %zu, expected %zu\n"
-			"*** Please report this to the Debian Apache maintainers\n",
-		sizeof(ino_t), sizeof(long));
+	fprintf(stderr, "***\n"
+			"*** 'ino_t' size is %zu, expected %zu\n"
+			"*** Please report this to the Debian Apache maintainers\n"
+			"***\n", s0, s1);
 	return 1;
 }
